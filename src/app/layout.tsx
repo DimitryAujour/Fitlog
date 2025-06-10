16adba75b33e4e3c27e1003694be9ef851117396
DimitryAujour <Dimitry.aujour@gmail.com>
2025-06-10 14:09:09 -0400

    moved adsense script to the head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import theme from '../theme/theme';
 import { Roboto } from 'next/font/google';
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
-import Script from 'next/script'; // 1. Import the Script component
+import Script from 'next/script';
 
 const roboto = Roboto({
     weight: ['300', '400', '500', '700'],
@@ -28,15 +28,16 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" className={roboto.variable}>
+        <head>
+            {/* --- MOVE THE SCRIPT HERE --- */}
+            <Script
+                async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3400469453529039"
+                crossOrigin="anonymous"
+                strategy="afterInteractive"
+            />
+        </head>
         <body>
-        {/* 2. Add the Script component here */}
-        <Script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3400469453529039"
-            crossOrigin="anonymous"
-            strategy="afterInteractive"
-        />
-
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
             <ThemeProvider theme={theme}>
                 <CssBaseline />
